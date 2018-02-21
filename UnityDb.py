3d8c59f2f8aaa16f1ca1b93d7108a73d7dd91bb6
Raj Desai <rajdesai@Rajs-MacBook-Pro.local>
2018-02-20 21:21:41 -0500

    Unity can get/put data from this server

--- UnityDb.py
+++ UnityDb.py
@@ -10,13 +10,22 @@ def hello():
     return "This is Unity database ec2 instance"
 
 #waaa has the results data
-app.config['MONGO_DBNAME'] = 'waaa'
+app.config['MONGO_DBNAME'] = 'caaaa'
 app.config['MONGO_HOST'] = '127.0.0.1'
 app.config['MONGO_PORT'] = 27017
 
+app.config['MONGO2_DBNAME'] = 'baaaa'
+app.config['MONGO2_HOST'] = '127.0.0.1'
+app.config['MONGO2_PORT'] = 27017
+
 mongo = PyMongo(app, config_prefix='MONGO')
+mongo2 = PyMongo(app, config_prefix='MONGO2')
+
 count=0
 #cdbb actual data is stored
+
+
+
 @app.route('/data', methods=['GET'])
 def get_all_datas():
     data = mongo.db.data
@@ -33,6 +42,42 @@ def get_all_datas():
     print "Getting " + str(output[0])
     #print "Type: "+ str(type(output))
     return jsonify(output[0])
+
+@app.route('/sketches', methods=['GET'])
+def get_all_sketches():
+    data = mongo2.db.data
+    output = []
+    for s in data.find():
+        #s.pop('_id')
+        print s
+        output.append(s['vector'])
+        #{'vector' : s['vector']}
+    #output=output.encode("ascii", "replace")
+    output = json.dumps(output)
+    output = json.loads(output)
+    #type(loaded_r)  # Output dict
+    print "Getting " + str(output)
+    #print "Type: "+ str(type(output))
+    return jsonify({"results":output})
+
+
+@app.route('/sketches/<int:count>', methods=['GET'])
+def get_all_sketche(count):
+    print "got here"
+    data = mongo2.db.data
+    output = []
+    for s in data.find():
+        # s.pop('_id')
+        print s
+        output.append(s['vector'])
+
+    #output={'results': output}
+    output = json.dumps(output)
+    output = json.loads(output)
+    print "Getting " + str(output)
+    #print "The first one:" + str(output[0])
+    return jsonify(output[count])
+
 
 '''@app.route('/data/<int:count>', methods=['GET'])
 def get_all_data(count):
@@ -75,27 +120,31 @@ def do_delete(count):
         print (s['vector']['check'])
 
     output.remove(output[count])
-    return jsonify(output)
+    return jsonify(output)'''
 
-@app.route('/data/<int:count>', methods=['PUT'])
+@app.route('/sketches/<int:count>', methods=['PUT'])    #"""PUT"""
 def update_task(count):
-    data = mongo.db.data
+    print "\n\n PUT \n\n"
+    data = mongo2.db.data
     reqdata = json.loads(request.data)
     vector = reqdata
-    postall_id = data.insert({'vector': vector})
-    new_postall = data.find_one({'_id': postall_id})
+    print vector
+    postall_id = data.update_one({'_id': vector['id']}, {'$set': {'vector': vector}})
+    #postall_id = data.insert({'vector': vector})
+    print postall_id.matched_count
+    new_postall = data.find_one({'_id': vector['id']})
     output = {'vector': new_postall['vector']}
     print "Posting " + str(output)
-    return jsonify({'results': output})
+    return jsonify(output)
     #data = mongo.db.data
-    output = []
-    for s in mongo.db.data.find():
-        if s['vector']['id']==count:
-            s['vector']['check']=1
-            output.append(s['vector'])
+    # output = []
+    # for s in mongo2.db.data.find():
+    #     if s['vector']['id']==count:
+    #         s['vector']['check']=5000
+    #         output.append(s['vector'])
 
-    #print output[count]
-    return jsonify(output)'''
+    # #print output[count]
+    # return jsonify(output)
 
 @app.route('/data', methods=['POST'])
 def add_data():
@@ -116,6 +165,80 @@ def add_data():
     output = {'num': new_postall['num']}
     print "Posting " + str(output)
     return jsonify({'data': output})
+
+@app.route('/sketches', methods=['POST'])
+def add_sketches():
+    data = mongo2.db.data
+    reqdata = json.loads(request.data)
+    print reqdata
+    vector = reqdata['results']
+    print "\n\n\nhere we go\n\n\n"
+    print vector
+    print "\n\n\nthere we go\n\n\n"    
+    #vector = vector['vector']
+    print type(vector)
+    #vector = json.loads(vector)
+    index=0
+    datastored=[]
+    print "Length of vect list" + str(len(vector))
+    while index < len(vector):
+        #vector={'data': vector[index]}
+        print "my Vector \n\n\n" + str(vector[index]) + "\n\n\n"
+        #vector = vector['data']
+        #myaxis=vector['axis']
+        print vector[index]['id']
+        print 
+        try:
+            postall_id = data.insert({'_id':vector[index]['id'],'vector': vector[index]})
+        except:
+            postall_id = data.update_one({'_id': vector[index]['id']}, {'$set': {'vector': vector[index]}})
+        print postall_id
+        new_postall = data.find_one({'_id': vector[index]['id'] })
+        print new_postall
+        output = {'vector': new_postall['vector']}
+        datastored.append(output)
+        index+=1
+    return jsonify(datastored)
+
+
+@app.route('/sketches/<int:count>', methods=['POST'])
+def change_data(count):
+    print "Posting in post"
+    data = mongo2.db.data
+    reqdata = request.data
+    print reqdata
+    print type(reqdata)
+    if reqdata:
+        print "Python"
+        reqdata = json.loads(request.data)
+        vector = reqdata['data']
+        postall_id = data.update_one({'_id': vector['id']}, {'$set': {'vector': vector}})
+        print postall_id.matched_count
+        new_postall = data.find_one({'_id': vector['id']})
+        print new_postall
+        output = {'vector': new_postall['vector']}
+        print "Posting " + str(output)
+        return jsonify({'data': output})
+    else:
+        # print myDict
+        # reqdata = json.loads(request.form)
+        print "Unity"
+        reqdata = dict(request.form)
+        #reqdata={"data":reqdata}
+        print "req data"
+        print reqdata
+        vector = reqdata['data']
+        print "vector"
+        # vector=dict(vector[0])
+        vector = json.loads(vector[0])
+        print vector
+        postall_id = data.update_one({'_id': vector['id']}, {'$set': {'vector': vector}})
+        print postall_id.matched_count
+        new_postall = data.find_one({'_id': vector['id']})
+        print new_postall
+        output = {'vector': new_postall['vector']}
+        print "Posting " + str(output)
+        return jsonify({'data': output})
 
 '''@app.route('/data/<int:count>', methods=['POST'])
 def change_data(count):
